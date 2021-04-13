abc001f2a0e23f314ff0e304362845bf1e8ee9ff
liouville12138 <58822438+liouville12138@users.noreply.github.com>
2021-04-13 19:19:42 +0800

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,9 +1,13 @@
 import os
-from PIL import Image
+import pickle
+import torch
 import glob
+import numpy as np
+from PIL import Image
 from torchvision import transforms
+from sklearn.preprocessing import LabelEncoder
 
-class LoadImages(object,):
+class LoadImages(object):
     def __init__(self):
         self.data = []
 
@@ -24,3 +28,26 @@ class LoadImages(object,):
             for key in i:
                 out.append({key: transforms.ToTensor()(i[key])})
         return out
+
+if __name__ == '__main__':
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    label = []
+    data = []
+
+    cat_path = glob.glob(current_path + "/data_set/cat/*.*")  # 匹配所有的符合条件的文件，并将其以list的形式返回
+    for i in cat_path:
+        label.append("cat")
+        test = Image.open(i)
+        data.append(transforms.ToTensor()(Image.open(i)))
+
+    others_path = glob.glob(current_path + "/data_set/others/*.*")  # 匹配所有的符合条件的文件，并将其以list的形式返回
+    for repeat in range(10):
+        for i in others_path:
+            label.append("others")
+            data.append(transforms.ToTensor()(Image.open(i).convert("RGB")))
+
+    with open("data_set/data_set.pkl", "wb") as data_set:
+        pickle.dump(label, data_set)
+        pickle.dump(data, data_set)
+
+
